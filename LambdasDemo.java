c1691c92a6de1cc01268a36c8daac36b02511a57
Bhavesh Kumar <bhawishdhanjanii@gmail.com>
2023-03-26 00:09:58 +0500

    Implimenting Interface using anonymous inner class

--- LambdasDemo.java
+++ LambdasDemo.java
@@ -2,7 +2,12 @@ package Lambdas;
 
 public class LambdasDemo {
     public static void show(){
-        greet(new ConsolePrinter());
+        greet(new Printer() {
+            @Override
+            public void print(String message) {
+                System.out.println(message);
+            }
+        });
     }
     public static void greet(Printer printer){
         printer.print("Good Morning");
--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ package Lambdas;
 
 public class Main {
     public static void main(String[] args) {
+        LambdasDemo.show();
 
     }
 }
