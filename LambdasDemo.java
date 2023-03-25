fbf1acd6093f64d4b46b5023e1da183cc4a1ef55
Bhavesh Kumar <bhawishdhanjanii@gmail.com>
2023-03-26 00:15:33 +0500

    Implimenting Interface using lamda expresion

--- LambdasDemo.java
+++ LambdasDemo.java
@@ -2,12 +2,7 @@ package Lambdas;
 
 public class LambdasDemo {
     public static void show(){
-        greet(new Printer() {
-            @Override
-            public void print(String message) {
-                System.out.println(message);
-            }
-        });
+        greet((message)-> System.out.println(message));
     }
     public static void greet(Printer printer){
         printer.print("Good Morning");
