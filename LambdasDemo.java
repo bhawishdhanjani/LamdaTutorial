7f26c2508d506c32db4eb449b3916c6ffaccf79a
Bhavesh Kumar <bhawishdhanjanii@gmail.com>
2023-03-27 14:58:08 +0500

    Tutorial on method refrences

--- LambdasDemo.java
+++ LambdasDemo.java
@@ -1,9 +1,22 @@
 package Lambdas;
 
 public class LambdasDemo {
+    public LambdasDemo(String message) {
+    }
+
+    public void print(String message){
+
+    }
     public static void show(){
-        Printer printer = message-> System.out.println(message);
-        greet(printer);
+//        Printer printer = System.out::println;
+//        greet(System.out::println);
+//        greet(message -> print(message));
+//        greet(LambdasDemo::print);
+//        var demo = new LambdasDemo();
+//        greet(message -> demo.print(message));
+//        greet(demo::print);
+        greet(message -> new LambdasDemo(message));
+        greet(LambdasDemo::new);
     }
     public static void greet(Printer printer){
         printer.print("Good Morning");
