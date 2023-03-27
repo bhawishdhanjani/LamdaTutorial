a635e8b7dddfbb34d3502b082b60958b898b1e53
Bhavesh Kumar <bhawishdhanjanii@gmail.com>
2023-03-27 17:06:07 +0500

    Tutorial on Binary Operator INterface

---
+++ BinaryOperatorInterface.java
@@ -0,0 +1,14 @@
+package Lambdas;
+
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+
+public class BinaryOperatorInterface {
+    public static void show(){
+        BinaryOperator<Integer> add = (a,b) -> a+b;
+        Function<Integer,Integer> square = a -> a*a ;
+        var result = add.andThen(square).apply(1,2);
+        System.out.println(result);
+
+    }
+}
