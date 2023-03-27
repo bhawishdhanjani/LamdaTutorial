dce18449672f0c66f9c93ba307e2e681a332b3ed
Bhavesh Kumar <bhawishdhanjanii@gmail.com>
2023-03-27 16:27:06 +0500

    Tutorial on Composig Function

--- FunctionInterfaceDemo.java
+++ FunctionInterfaceDemo.java
@@ -7,6 +7,10 @@ public class FunctionInterfaceDemo {
         Function<String,Integer> map = str -> str.length();
         int length = map.apply("Hello i am Bhawish.");
         System.out.println(length);
+        Function<String,String> replaceColon = str -> str.replace(":","=");
+        Function<String,String> addBrace = str -> "{"+str+"}";
+        var result= replaceColon.andThen(addBrace).apply("20CS065:BhawishKumar");
+        System.out.println(result);
 
     }
 }
